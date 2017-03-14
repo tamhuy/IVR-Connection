add86c47955397901c638d7824c4f809cc9b9bf9
Dragvoll User <Dragvoll@NTNU03688.win.ntnu.no>
2017-03-14 13:51:00 +0100

    - Add all necessary configuration for non-spatialized voice chat

---
+++ Source/Master_Project/VoiceSource.cpp
@@ -0,0 +1,90 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Master_Project.h"
+#include "VoiceSource.h"
+
+#include "OnlineSubsystemUtils.h"
+
+// Sets default values for this component's properties
+UVoiceSource::UVoiceSource()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+UVoiceSource::~UVoiceSource()
+{
+	GetWorld()->GetTimerManager().ClearTimer(m_timerHandle);
+	//GetWorld()->GetTimerManager().ClearTimer(m_timerFrequency);
+}
+
+// Called when the game starts
+void UVoiceSource::BeginPlay()
+{
+	auto alib = LoadLibrary(L"dsound");
+
+	if (alib)
+	{
+		UE_LOG(LogGarbage, Warning, TEXT("Loaded module!"));
+	}
+	else {
+		UE_LOG(LogGarbage, Warning, TEXT("Failed to load DLL"));
+	}
+
+	Super::BeginPlay();
+
+	auto online = IOnlineSubsystem::Get("Steam");
+	if (!online)
+	{
+		UE_LOG(LogGarbage, Warning, TEXT("Failed to online"));
+		return;
+	}
+
+	auto voice = online->GetVoiceInterface();
+	if (!voice.IsValid())
+	{
+		UE_LOG(LogGarbage, Warning, TEXT("Failed to voice"));
+		return;
+	}
+
+	voice->StartNetworkedVoice(0);
+	voice->RegisterLocalTalker(0);
+	
+	m_voice = voice;
+
+	UE_LOG(LogGarbage, Warning, TEXT("LOL"));
+
+	GetWorld()->GetTimerManager().SetTimer(m_timerHandle, [&]()
+	{
+		if (m_voice.IsValid())
+		{
+			m_voice->Tick(1.f / 22500.f);
+
+			//m_voicePacket = m_voice->GetLocalPacket(0);
+		}
+	}, 1.f / 22500.f, true, 0.5f);
+	/*
+	GetWorld()->GetTimerManager().SetTimer(m_timerFrequency, [&]()
+	{
+		if (m_voicePacket.IsValid())
+		{
+			m_voicePacket->Serialize(m_freqArchive);
+		}
+	}, 1.f / 10.f, true, 0.5f);
+	*/
+}
+
+
+// Called every frame
+void UVoiceSource::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	
+
+	// ...
+}
+
---
+++ Source/Master_Project/VoiceSource.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "OnlineSubsystemUtils.h"
+
+#include "VoiceSource.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class MASTER_PROJECT_API UVoiceSource : public UActorComponent
+{
+	GENERATED_BODY()
+
+	IOnlineVoicePtr m_voice;
+
+	//TSharedPtr<FVoicePacket> m_voicePacket;
+
+	FTimerHandle m_timerHandle;
+	/*
+	FTimerHandle m_timerFrequency;
+
+	FArchive m_freqArchive;
+	*/
+
+public:	
+	// Sets default values for this component's properties
+	UVoiceSource();
+	~UVoiceSource();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+};
