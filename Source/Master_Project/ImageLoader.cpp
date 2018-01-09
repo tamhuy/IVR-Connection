a74e3d0258e39bcb0f16050e9bbd52343a8a4aa0
unknown <dragvoll@NTNU05953.win.ntnu.no>
2018-01-09 11:52:34 +0100

    Fixed image loading

--- Source/Master_Project/ImageLoader.cpp
+++ Source/Master_Project/ImageLoader.cpp
@@ -6,13 +6,13 @@
 #include "Runtime/ImageWrapper/Public/IImageWrapperModule.h"
 #include "RenderUtils.h"
 #include "Engine/Texture2D.h"
- 
+
 // Change the UE_LOG log category name below to whichever log category you want to use.
 #define UIL_LOG(Verbosity, Format, ...)	UE_LOG(LogTemp, Verbosity, Format, __VA_ARGS__)
- 
+
 // Module loading is not allowed outside of the main thread, so we load the ImageWrapper module ahead of time.
-//static IImageWrapperModule& ImageWrapperModule = FModuleManager::LoadModuleChecked<IImageWrapperModule>(TEXT("ImageWrapper"));
- 
+static IImageWrapperModule& ImageWrapperModule = FModuleManager::LoadModuleChecked<IImageWrapperModule>(TEXT("ImageWrapper"));
+
 UImageLoader* UImageLoader::LoadImageFromDiskAsync(UObject* Outer, const FString& ImagePath)
 {
 	// This simply creates a new ImageLoader object and starts an asynchronous load.
@@ -20,7 +20,7 @@ UImageLoader* UImageLoader::LoadImageFromDiskAsync(UObject* Outer, const FString
 	Loader->LoadImageAsync(Outer, ImagePath);
 	return Loader;
 }
- 
+
 void UImageLoader::LoadImageAsync(UObject* Outer, const FString& ImagePath)
 {
 	// The asynchronous loading operation is represented by a Future, which will contain the result value once the operation is done.
@@ -36,14 +36,14 @@ void UImageLoader::LoadImageAsync(UObject* Outer, const FString& ImagePath)
 		}
 	});
 }
- 
+
 TFuture<UTexture2D*> UImageLoader::LoadImageFromDiskAsync(UObject* Outer, const FString& ImagePath, TFunction<void()> CompletionCallback)
 {
 	// Run the image loading function asynchronously through a lambda expression, capturing the ImagePath string by value.
 	// Run it on the thread pool, so we can load multiple images simultaneously without interrupting other tasks.
 	return Async<UTexture2D*>(EAsyncExecution::ThreadPool, [=]() { return LoadImageFromDisk(Outer, ImagePath); }, CompletionCallback);
 }
- 
+
 UTexture2D* UImageLoader::LoadImageFromDisk(UObject* Outer, const FString& ImagePath)
 {
 	// Check if the file exists first
@@ -52,7 +52,7 @@ UTexture2D* UImageLoader::LoadImageFromDisk(UObject* Outer, const FString& Image
 		UIL_LOG(Error, TEXT("File not found: %s"), *ImagePath);
 		return nullptr;
 	}
- 
+
 	// Load the compressed byte data from the file
 	TArray<uint8> FileData;
 	if (!FFileHelper::LoadFileToArray(FileData, *ImagePath))
@@ -60,16 +60,15 @@ UTexture2D* UImageLoader::LoadImageFromDisk(UObject* Outer, const FString& Image
 		UIL_LOG(Error, TEXT("Failed to load file: %s"), *ImagePath);
 		return nullptr;
 	}
- 
+
 	// Detect the image type using the ImageWrapper module
-	IImageWrapperModule& ImageWrapperModule = FModuleManager::LoadModuleChecked<IImageWrapperModule>(TEXT("ImageWrapper"));
 	EImageFormat ImageFormat = ImageWrapperModule.DetectImageFormat(FileData.GetData(), FileData.Num());
 	if (ImageFormat == EImageFormat::Invalid)
 	{
 		UIL_LOG(Error, TEXT("Unrecognized image file format: %s"), *ImagePath);
 		return nullptr;
 	}
- 
+
 	// Create an image wrapper for the detected image format
 	TSharedPtr<IImageWrapper> ImageWrapper = ImageWrapperModule.CreateImageWrapper(ImageFormat);
 	if (!ImageWrapper.IsValid())
@@ -77,22 +76,22 @@ UTexture2D* UImageLoader::LoadImageFromDisk(UObject* Outer, const FString& Image
 		UIL_LOG(Error, TEXT("Failed to create image wrapper for file: %s"), *ImagePath);
 		return nullptr;
 	}
- 
+
 	// Decompress the image data
 	const TArray<uint8>* RawData = nullptr;
-	//ImageWrapper->SetCompressed(FileData.GetData(), FileData.Num());
-	//ImageWrapper->GetRaw(ERGBFormat::BGRA, 8, RawData);
+	ImageWrapper->SetCompressed(FileData.GetData(), FileData.Num());
+	ImageWrapper->GetRaw(ERGBFormat::BGRA, 8, RawData);
 	if (RawData == nullptr)
 	{
 		UIL_LOG(Error, TEXT("Failed to decompress image file: %s"), *ImagePath);
 		return nullptr;
 	}
- 
-	// Create the texture and upload the uncompressed image data  
+
+	// Create the texture and upload the uncompressed image data
 	FString TextureBaseName = TEXT("Texture_") + FPaths::GetBaseFilename(ImagePath);
 	return CreateTexture(Outer, *RawData, ImageWrapper->GetWidth(), ImageWrapper->GetHeight(), EPixelFormat::PF_B8G8R8A8, FName(*TextureBaseName));
 }
- 
+
 UTexture2D* UImageLoader::CreateTexture(UObject* Outer, const TArray<uint8>& PixelData, int32 InSizeX, int32 InSizeY, EPixelFormat InFormat, FName BaseName)
 {
 	// Shamelessly copied from UTexture2D::CreateTransient with a few modifications
@@ -103,16 +102,16 @@ UTexture2D* UImageLoader::CreateTexture(UObject* Outer, const TArray<uint8>& Pix
 		UIL_LOG(Warning, TEXT("Invalid parameters specified for UImageLoader::CreateTexture()"));
 		return nullptr;
 	}
- 
+
 	// Most important difference with UTexture2D::CreateTransient: we provide the new texture with a name and an owner
 	FName TextureName = MakeUniqueObjectName(Outer, UTexture2D::StaticClass(), BaseName);
 	UTexture2D* NewTexture = NewObject<UTexture2D>(Outer, TextureName, RF_Transient);
- 
+
 	NewTexture->PlatformData = new FTexturePlatformData();
 	NewTexture->PlatformData->SizeX = InSizeX;
 	NewTexture->PlatformData->SizeY = InSizeY;
 	NewTexture->PlatformData->PixelFormat = InFormat;
- 
+
 	// Allocate first mipmap and upload the pixel data
 	int32 NumBlocksX = InSizeX / GPixelFormats[InFormat].BlockSizeX;
 	int32 NumBlocksY = InSizeY / GPixelFormats[InFormat].BlockSizeY;
@@ -123,7 +122,7 @@ UTexture2D* UImageLoader::CreateTexture(UObject* Outer, const TArray<uint8>& Pix
 	void* TextureData = Mip->BulkData.Realloc(NumBlocksX * NumBlocksY * GPixelFormats[InFormat].BlockBytes);
 	FMemory::Memcpy(TextureData, PixelData.GetData(), PixelData.Num());
 	Mip->BulkData.Unlock();
- 
+
 	NewTexture->UpdateResource();
 	return NewTexture;
 }
